873e039a261d201ad2a60dcaff9921b0b78b9c2c
Elzat-developer <fergenson.2020@gmail.com>
2026-03-30 16:33:54 +0500

    fix import

--- pages/AdminImportPage.tsx
+++ pages/AdminImportPage.tsx
@@ -226,7 +226,7 @@ export const AdminImportPage: React.FC = () => {
 			</div>
 
 			{/* Content area with normal background */}
-			<div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
 
 				{/* Import Tab Content */}
 				{activeTab === 'import' && (
